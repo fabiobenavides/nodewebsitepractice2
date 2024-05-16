870a19b48a1b3f95c4ea5b3a338204346eaac8b5
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-05-16 15:56:53 -0600

    First Page

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('A node ws');
