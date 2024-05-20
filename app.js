d3616ee47249edb640811ec21109042fbfe03d6b
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-05-20 08:56:03 -0600

    ejs array example

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const { title } = require('process');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000; //default
 const app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
+//app.use(express.static(path.join(__dirname, '/public/')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('A node ws');
+    res.render('index', {title:'Welcome to Fabiomantics', data: ['a', 'b', 'c']});
 })
 
 app.listen(PORT, () => {
