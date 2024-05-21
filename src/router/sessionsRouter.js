b9ad0a66eab1c7552e7975e4dbe2ed8608260e5f
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-05-21 11:45:13 -0600

    Insert and read from MongoDB

--- src/router/sessionsRouter.js
+++ src/router/sessionsRouter.js
@@ -1,21 +1,63 @@
 const express = require('express');
-const sessionData = require('../data/sessions.json');
+const debug = require('debug')('app:sessions');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const sessionsRouter = express.Router();
 
 
 sessionsRouter.route('/')
     .get((req, res) => {
-        res.render('sessions', {
-            sessionData
-        });
+
+        const url = 'mongodb://user:pass@localhost:27017';
+        const dbName = 'fabiotesting';
+
+        (async function mongo(){
+
+            let client;
+            
+            try {
+                debug('trying to connected to mongo');
+                client = await MongoClient.connect(url);
+                debug('connected to mongo');
+                const db = client.db(dbName);
+                const session = await db.collection('sessions').find().toArray();
+
+                res.render('sessions', { sessionData: session});
+
+            }
+            catch (error) {
+                debug(error.stack);
+                res.send(error.stack);
+            }
+
+        }());
     });
 sessionsRouter.route('/:id')
     .get((req, res) => {
         const id = req.params.id;
-        res.render('session', {
-            session: sessionData[id]
-        });
+        const url = 'mongodb://user:pass@localhost:27017';
+        const dbName = 'fabiotesting';
+
+        (async function mongo(){
+
+            let client;
+            
+            try {
+                debug('trying to connected to mongo');
+                client = await MongoClient.connect(url);
+                debug('connected to mongo');
+                const db = client.db(dbName);
+                const session = await db.collection('sessions').findOne({_id: new ObjectId(id)});
+
+                res.render('session', {session});
+
+            }
+            catch (error) {
+                debug(error.stack);
+                res.send(error.stack);
+            }
+
+        }());
     });
 
 module.exports = sessionsRouter;
